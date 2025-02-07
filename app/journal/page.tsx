a2c0627ddd0526ddfccd2b2b08bd9eb134616971
Sudipta Dandapat <sudipta@meta.com>
2025-02-07 20:54:01 +0000

    showing spinner when changing date in journal

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -14,9 +14,12 @@ import { Button } from "@/components/ui/button";
 import { Loader2, Save } from "lucide-react";
 import axios from 'axios';
 import { useToast } from "@/hooks/use-toast";
+import LoadingSpinner from "@/components/loadingSpinner";
+import { formatToLocalDate } from "@/lib/utils";
 
 const JournalPage = () => {
     const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+    const [isFetching, setIsFetching] = useState<boolean>(false);
     const [isSaving, setIsSaving] = useState<boolean>(false);
     const { toast } = useToast();
 
@@ -31,11 +34,13 @@ const JournalPage = () => {
     // Fetch journal when date changes
     useEffect(() => {
         const fetchJournalEntry = async () => {
+            setIsFetching(true);
             try {
                 const formattedDate = selectedDate.toISOString().split('T')[0];
                 const res = await axios.get(`/api/journal/view?date=${formattedDate}`);
 
                 console.log("Journal Entry:", res.data); // ✅ Debugging
+                setIsFetching(false);
 
                 if (res.status === 200 && res.data.content) {
                     form.setValue("content", res.data.content);
@@ -44,6 +49,7 @@ const JournalPage = () => {
                 }
             } catch (error) {
                 console.error("Error fetching journal entry:", error);
+                setIsFetching(false);
             }
         };
 
@@ -121,22 +127,28 @@ const JournalPage = () => {
                 <Card className="w-full lg:h-[413px] h-[300px] shadow-xl dark:shadow-2xl rounded-lg flex flex-col">
                     <CardContent className="flex flex-col p-6">
                         <h2 className="text-2xl font-semibold text-gray-900 dark:text-white text-center">Write your Journal</h2>
+                        <h4 className="text-center pb-2">{formatToLocalDate(selectedDate)}</h4>
                         <Form {...form}>
                             <form onSubmit={(e) => {
                                 e.preventDefault();
                                 handleSave();
                             }} className="flex flex-col flex-grow space-y-4">
                                 <FormField control={form.control} name="content" render={({ field }) => (
-                                    <FormItem className="flex-grow">
-                                        <Textarea {...field} placeholder="Express your day..."
+                                    <FormItem className="flex-grow relative">
+                                        {isFetching ?
+                                            <div className="absolute top-1/2 left-1/2 transform -translate-x-2/5 -translate-y-1/2">
+                                                <LoadingSpinner />
+                                            </div>
+                                            : null}
+                                        <Textarea {...field} placeholder="Express your day..." disabled={isFetching}
                                             className="w-full h-60 p-4 text-lg rounded-md border dark:border-gray-600 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-300" />
                                     </FormItem>
                                 )} />
                                 <div className="flex justify-end">
-                                    <Button 
-                                        type="submit" 
-                                        className="self-end" 
-                                        size={'lg'} 
+                                    <Button
+                                        type="submit"
+                                        className="self-end"
+                                        size={'lg'}
                                         disabled={selectedDate.getDate() !== new Date().getDate()}>
                                         {isSaving ? <Loader2 className="animate-spin" /> : <Save />}
                                     </Button>
