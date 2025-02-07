615bc427927e2377adb31e4ac258ccae0a9d2e29
rohitcodism <rohitpaulhhs04@gmail.com>
2025-02-08 04:04:16 +0530

    Change date field in journal schema from date to string

--- schemas/journal.schema.ts
+++ schemas/journal.schema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 
 
 export const journalSchema = z.object({
-    date: z.date(),
+    date: z.string(),
     content: z.string().min(50, "Minimum 50 words required.").max(500, "Journal can contain maximum 500 words.")
 })
