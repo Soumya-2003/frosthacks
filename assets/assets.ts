1cd55bba78af9bda4948c7d1a8c5e397fbba7605
rohitcodism <rohitpaulhhs04@gmail.com>
2025-02-07 20:15:06 +0530

    add response entry type and asset imports for enhanced functionality

---
+++ assets/assets.ts
@@ -0,0 +1,17 @@
+import notepadSvg from './1b50937c-7f70-4a26-9f8c-b12773b05404.svg';
+import notepad2 from './1b50937c-7f70-4a26-9f8c-b12773b05404.jpg';
+import notepad3 from './notepad3.png';
+import journal from './journal.png';
+import mood from './mood.png'
+import report from './report.png';
+import social from './social.png';
+
+export {
+    notepadSvg,
+    notepad2,
+    notepad3,
+    journal,
+    mood,
+    report,
+    social
+}
