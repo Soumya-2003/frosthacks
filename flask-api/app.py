18c7c59b855ccc23721a5013dd6ec7ab151ac949
rohitcodism <rohitpaulhhs04@gmail.com>
2025-02-08 22:51:32 +0530

    Enhance error handling in Flask API by logging detailed error messages and returning a generic internal server error response

--- flask-api/app.py
+++ flask-api/app.py
@@ -109,7 +109,8 @@ def analyze_sentence():
         return jsonify(sentiment_results), 200
     
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        print(f"Error in Flask API: {e}")  # Log exact error
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 @app.route('/analyze-weekly-data', methods=['POST'])
 def analyze_weekly_data():
